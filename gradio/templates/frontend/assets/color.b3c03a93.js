9ece2701ae8de16df6adc87ae615b56bc954dc05
sch-28 <sch-28@users.noreply.github.com>
2022-11-16 23:51:14 +0000

    Deployed build

---
+++ gradio/templates/frontend/assets/color.b3c03a93.js
@@ -0,0 +1,2 @@
+import{af as o}from"./index.ccb76436.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color.b3c03a93.js.map
---
+++ gradio/templates/frontend/assets/index.359032cb.js
@@ -0,0 +1,2 @@
+import{S as s,i as e,s as o}from"./index.ccb76436.js";class a extends s{constructor(t){super(),e(this,t,null,null,o,{})}}var r=a;const i=["static"];export{r as Component,i as modes};
+//# sourceMappingURL=index.359032cb.js.map
