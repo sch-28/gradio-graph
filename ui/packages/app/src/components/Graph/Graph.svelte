b149b7a48fb1227a2932aa2f167f3925c3c371ff
sch-28 <jansch2803@googlemail.com>
2022-11-28 19:01:55 +0100

    feat: endpoint, rate_limit, size_limit exposed

--- ui/packages/app/src/components/Graph/Graph.svelte
+++ ui/packages/app/src/components/Graph/Graph.svelte
@@ -5,11 +5,23 @@
 	export let label: string;
 	export let elem_id: string = "";
 	export let visible: boolean = true;
+
 	export let value: string = "";
+	export let endpoint: string = "https://query.wikidata.org/sparql";
+	export let rate_limit: number = 5;
+	export let size_limit: number = 100;
 
 	const dispatch = createEventDispatcher<{ change: undefined }>();
 
 	$: label, dispatch("change");
 </script>
 
-<Graph on:change {elem_id} {value} {visible} />
+<Graph
+	on:change
+	{elem_id}
+	root={value}
+	{visible}
+	{endpoint}
+	{rate_limit}
+	{size_limit}
+/>
