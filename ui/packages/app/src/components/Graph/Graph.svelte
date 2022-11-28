45ad642a1f5779f83db7570392e63b2fa614e755
sch-28 <jansch2803@googlemail.com>
2022-11-28 20:58:24 +0100

    fix: adjusted graph interface

--- ui/packages/app/src/components/Graph/Graph.svelte
+++ ui/packages/app/src/components/Graph/Graph.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 	import { Graph } from "../../../../graph/";
+	import ApiDocs from "../../ApiDocs.svelte";
+	import Accordion from "../Accordion/Accordion.svelte";
+	import Audio from "../Audio/Audio.svelte";
 
 	export let label: string;
 	export let elem_id: string = "";
 	export let visible: boolean = true;
 
-	export let value: string = "";
-	export let endpoint: string = "https://query.wikidata.org/sparql";
-	export let rate_limit: number = 5;
-	export let size_limit: number = 100;
-
+	export let value: {
+		value: string;
+		endpoint: string;
+		rate_limit: number;
+		size_limit: number;
+	} = {
+		value: "",
+		endpoint: "https://query.wikidata.org/sparql",
+		rate_limit: 5,
+		size_limit: 100
+	};
 	const dispatch = createEventDispatcher<{ change: undefined }>();
 
 	$: label, dispatch("change");
 </script>
 
-<Graph
-	on:change
-	{elem_id}
-	root={value}
-	{visible}
-	{endpoint}
-	{rate_limit}
-	{size_limit}
-/>
+<Graph on:change {elem_id} {visible} {...value} />
